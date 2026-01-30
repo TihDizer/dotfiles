d782d3f352c696183407c52e6eb513a0a4c031d1
tihdizer <tihdizer@gmail.com>
2026-01-30 15:28:43 +0300

    feat: update config

--- modules/desktop/sunshine.nix
+++ modules/desktop/sunshine.nix
@@ -3,28 +3,17 @@
 {
   environment.systemPackages = with pkgs; [
     pkgs.sunshine
-    pkgs.moonlight-qt # for testing purposes.
+    pkgs.moonlight-qt
   ];
 
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
-  networking.firewall = {
-    allowedTCPPorts = [
-      47984
-      47989
-      47990
-      48010
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 47998;
-        to = 48000;
-      }
-      #{ from = 8000; to = 8010; }
-    ];
+  services.sunshine = {
+    enable = true;
+    openFirewall = true;
+    capSysAdmin = true;
+    autoStart = false;
+    settings = {
+      capture = "kms";
+      output_name = "DP-3";
+    };
   };
 }
