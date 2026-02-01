f5f51b7dacff42ff5d45a3dc649d6238ecc62e3b
tihdizer <tihdizer@gmail.com>
2026-02-01 18:04:37 +0300

    feat: bluetooth GUI

--- modules/core/bluetooth.nix
+++ modules/core/bluetooth.nix
@@ -8,9 +8,12 @@
 {
   # Bluetooth
   hardware.bluetooth.enable = true;
-  systemd.user.services.obex = {
-    enable = true;
-    description = "Bluetooth OBEX daemon";
-    serviceConfig.ExecStart = "${pkgs.bluez}/libexec/bluetooth/obexd --root=%h/Downloads --auto-accept";
-  };
+
+  # Blueman — GUI
+  services.blueman.enable = true;
+  # systemd.user.services.obex = {
+  #   enable = true;
+  #   description = "Bluetooth OBEX daemon";
+  #   serviceConfig.ExecStart = "${pkgs.bluez}/libexec/bluetooth/obexd --root=%h/Downloads --auto-accept";
+  # };
 }
