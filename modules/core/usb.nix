4bebb429adb981ca25967d8cf958e0f741d49c77
tihdizer <tihdizer@gmail.com>
2026-02-02 15:47:56 +0300

    feat: add default ntfs3g for udisk2

--- modules/core/usb.nix
+++ modules/core/usb.nix
@@ -13,4 +13,10 @@
   environment.systemPackages = with pkgs; [
     ntfs3g
   ];
+
+  environment.etc."udisks2/mount_options.conf".text = ''
+    [defaults]
+    ntfs_defaults=uid=%u,gid=%g,prealloc
+    ntfs_allow=uid,gid,dmask,fmask,prealloc
+  '';
 }
