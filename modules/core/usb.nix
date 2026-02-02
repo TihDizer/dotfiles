b65cc5af7bcb58aa061f94567e2157a86ac28104
tihdizer <tihdizer@gmail.com>
2026-02-02 15:54:27 +0300

    fix: remove options

--- modules/core/usb.nix
+++ modules/core/usb.nix
@@ -14,9 +14,5 @@
     ntfs3g
   ];
 
-  environment.etc."udisks2/mount_options.conf".text = ''
-    [defaults]
-    ntfs_defaults=uid=%u,gid=%g,prealloc
-    ntfs_allow=uid,gid,dmask,fmask,prealloc
-  '';
+  boot.blacklistedKernelModules = [ "ntfs3" ];
 }
