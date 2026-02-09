3c82434bc4e4fbe90e91be6162c2d19f41ef3626
tihdizer <tihdizer@gmail.com>
2026-02-09 19:38:19 +0300

    feat: add workspace ordering script

--- modules/home/tihdizer/niri/startup.nix
+++ modules/home/tihdizer/niri/startup.nix
@@ -7,7 +7,59 @@
     { sh = "sway-audio-idle-inhibit"; }
     { sh = "$POLKIT_BIN"; }
     { sh = "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all"; }
-    { command = [ "${pkgs.google-chrome}/bin/google-chrome" ]; }
-    { command = [ "${pkgs.telegram-desktop}/bin/Telegram" ]; }
   ];
+
+  systemd.user.services.niri-workspaces = {
+    Unit = {
+      Description = "Niri workspace order";
+      After = [
+        "graphical-session.target"
+        "niri.service"
+      ];
+      PartOf = [ "graphical-session.target" ];
+      Wants = [ "niri.service" ];
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
+    Service = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = pkgs.writeShellScript "niri-order" ''
+        #!/usr/bin/env bash
+        sleep 5
+        echo "Ordering workspaces..." | logger -t niri-order
+
+        # Primary
+        ${pkgs.niri}/bin/niri msg action focus-workspace "medias"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 1
+
+        ${pkgs.niri}/bin/niri msg action focus-workspace "devs"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 2
+
+        ${pkgs.niri}/bin/niri msg action focus-workspace "games"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 3
+
+        ${pkgs.niri}/bin/niri msg action focus-workspace "vms"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 4
+
+        # Secondary
+        ${pkgs.niri}/bin/niri msg action focus-workspace "chats"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 1
+
+        ${pkgs.niri}/bin/niri msg action focus-workspace "tools"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 2
+
+        ${pkgs.niri}/bin/niri msg action focus-workspace "launchers"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 3
+
+        ${pkgs.niri}/bin/niri msg action focus-workspace "tv"
+        ${pkgs.niri}/bin/niri msg action move-workspace-to-index 4
+
+        # After
+        ${pkgs.niri}/bin/niri msg action focus-workspace "chats"
+        ${pkgs.niri}/bin/niri msg action focus-workspace "medias"
+
+        echo "Niri workspaces ordered!" | logger -t niri-order
+      '';
+    };
+  };
 }
