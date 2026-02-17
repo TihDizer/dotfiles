b658e397f595d2692b05e269d3c224ea81a032dc
tihdizer <tihdizer@gmail.com>
2026-02-17 14:36:08 +0300

    feat: remove useless apps

--- modules/home/tihdizer/utils/apps.nix
+++ modules/home/tihdizer/utils/apps.nix
@@ -6,9 +6,7 @@
     obsidian # Markdown note app
     cosmic-files # Wayland file manager
     mission-center # System monitor GUI
-    mousai # Music recognition tool
 
     jellyfin-ffmpeg # Jellyfin FFmpeg build
-    bluetuith # Bluetooth TUI manager
   ];
 }
