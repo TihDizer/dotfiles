fc6d698d4ede7ed9d3b7b8281183eb139e5b19f2
tihdizer <tihdizer@gmail.com>
2026-02-26 20:16:21 +0300

    feat: add citrix workspace

--- modules/home/tihdizer/utils/apps.nix
+++ modules/home/tihdizer/utils/apps.nix
@@ -9,7 +9,7 @@
     obsidian # Markdown note app
     cosmic-files # Wayland file manager
     mission-center # System monitor GUI
-
+    citrix_workspace # Citrix Workspace
     jellyfin-ffmpeg # Jellyfin FFmpeg build
   ];
 }
