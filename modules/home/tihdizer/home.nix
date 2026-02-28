0e9f656cb625a24371ddde31b5b457a56395390d
tihdizer <tihdizer@gmail.com>
2026-02-28 19:47:37 +0300

    feat: symlinks for my mnts

--- modules/home/tihdizer/home.nix
+++ modules/home/tihdizer/home.nix
@@ -1,4 +1,5 @@
 {
+  config,
   ...
 }:
 
@@ -6,4 +7,16 @@
   home.username = "tihdizer";
   home.homeDirectory = "/home/tihdizer";
   home.stateVersion = "25.05";
+
+  home.file.".hdd1" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1";
+  };
+
+  # home.file.".hdd2" = {
+  #   source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1"; # подставь /mnt/hdd2 когда настроишь
+  # };
+
+  home.file.".nvme" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/mnt/nvme";
+  };
 }
