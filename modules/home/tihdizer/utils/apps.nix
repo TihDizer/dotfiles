b1fc4e763d33ddfd028bc6f1a82cc87d4d53ee6e
tihdizer <tihdizer@gmail.com>
2026-02-28 19:48:38 +0300

    feat: remove citrix

--- modules/home/tihdizer/utils/apps.nix
+++ modules/home/tihdizer/utils/apps.nix
@@ -9,7 +9,6 @@
     obsidian # Markdown note app
     cosmic-files # Wayland file manager
     mission-center # System monitor GUI
-    citrix_workspace # Citrix Workspace
     jellyfin-ffmpeg # Jellyfin FFmpeg build
     qbittorrent # BitTorrent client
     mpv # Video player
