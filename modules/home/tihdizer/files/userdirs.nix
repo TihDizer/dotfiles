2164956658a91774dd324d9c0687381b032f191b
tihdizer <tihdizer@gmail.com>
2026-03-01 14:38:12 +0300

    feat: all dirs

--- modules/home/tihdizer/files/userdirs.nix
+++ modules/home/tihdizer/files/userdirs.nix
@@ -28,15 +28,20 @@
     mkdir -p ~/games{nvme,hdd}
     mkdir -p ~/mnt/{usbs,nvme,storage,medias}
   '';
+
   home.file."mnt/storage".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1/tihdizer";
+  home.file."downloads".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1/tihdizer/downloads";
+  home.file."notes".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1/tihdizer/notes";
+  home.file."documents".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1/tihdizer/documents";
+  home.file."shared/storage".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1/shared";
 
   home.file."mnt/medias".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd2/tihdizer";
   home.file."medias".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd2/tihdizer/medias";
+  home.file."games/hdd".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd2/tihdizer/games";
+  home.file."shared/medias".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd2/shared";
 
   home.file."mnt/nvme".source = config.lib.file.mkOutOfStoreSymlink "/mnt/nvme/tihdizer";
   home.file."vms/nvme".source = config.lib.file.mkOutOfStoreSymlink "/mnt/nvme/tihdizer/vms";
   home.file."games/nvme".source = config.lib.file.mkOutOfStoreSymlink "/mnt/nvme/tihdizer/games";
-
-  # home.file."downloads".source =
-  #   config.lib.file.mkOutOfStoreSymlink "/hdd2/media/downloads";
+  home.file."projects".source = config.lib.file.mkOutOfStoreSymlink "/mnt/nvme/tihdizer/projects";
 }
