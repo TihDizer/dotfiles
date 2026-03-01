97270ef2248c1e88a84dec3ac589f77594cc7844
tihdizer <tihdizer@gmail.com>
2026-03-01 18:39:36 +0300

    fix: useless dirs create

--- modules/home/tihdizer/files/userdirs.nix
+++ modules/home/tihdizer/files/userdirs.nix
@@ -17,17 +17,17 @@
     publicShare = "$HOME/shared";
   };
 
-  home.activation.createDirs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p ~/medias/{music,pictures,videos}
-    mkdir -p ~/downloads/{git,}
-    mkdir -p ~/documents/templates
-    mkdir -p ~/shared{storage,medias}
-    mkdir -p ~/projects
-    mkdir -p ~/dotfiles
-    mkdir -p ~/notes ~/vms
-    mkdir -p ~/games{nvme,hdd}
-    mkdir -p ~/mnt/{usbs,nvme,storage,medias}
-  '';
+  # home.activation.createDirs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+  #   mkdir -p ~/medias/{music,pictures,videos}
+  #   mkdir -p ~/downloads/{git,}
+  #   mkdir -p ~/documents/templates
+  #   mkdir -p ~/shared{storage,medias}
+  #   mkdir -p ~/projects
+  #   mkdir -p ~/dotfiles
+  #   mkdir -p ~/notes ~/vms
+  #   mkdir -p ~/games{nvme,hdd}
+  #   mkdir -p ~/mnt/{usbs,nvme,storage,medias}
+  # '';
 
   home.file."mnt/storage".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1/tihdizer";
   home.file."downloads".source = config.lib.file.mkOutOfStoreSymlink "/mnt/hdd1/tihdizer/downloads";
