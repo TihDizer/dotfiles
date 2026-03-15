845ca6a19b61a34a7d036a4aff7e15de5a46a8b3
tihdizer <tihdizer@gmail.com>
2026-03-15 20:34:19 +0300

    feat: remove ntfs

--- modules/core/usb.nix
+++ modules/core/usb.nix
@@ -7,10 +7,4 @@
   # USB automount
   services.udisks2.enable = true;
   services.gvfs.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    ntfs3g
-  ];
-
-  boot.blacklistedKernelModules = [ "ntfs3" ];
 }
