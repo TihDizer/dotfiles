424cb2b9bf67ada51837bc003b4fc1f1dbd1b1da
tihdizer <tihdizer@gmail.com>
2026-03-22 22:08:52 +0300

    fix: reconnect

--- modules/core/bluetooth.nix
+++ modules/core/bluetooth.nix
@@ -8,6 +8,10 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
+  services.udev.extraRules = ''
+    ACTION=="change", SUBSYSTEM=="bluetooth", ATTR{authorized}="1"
+  '';
+
   boot.kernelParams = [
     "btusb.enable_autosuspend=n"
     "usbcore.autosuspend=-1"
