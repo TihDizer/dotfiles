a90594542ae9df028b20f3a4611755055dfbe0e4
tihdizer <tihdizer@gmail.com>
2026-04-01 20:03:35 +0300

    fix: portals

--- modules/home/tihdizer/niri/default.nix
+++ modules/home/tihdizer/niri/default.nix
@@ -31,8 +31,8 @@ in
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-wlr
+      # xdg-desktop-portal-gtk
+      # xdg-desktop-portal-wlr
     ];
     config = {
       common.default = [ "gnome" ];
