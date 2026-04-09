d1fff356da2587c4d3e06edac09395726bf3562e
tihdizer <tihdizer@gmail.com>
2026-04-09 20:48:11 +0300

    chore: delete go and python

--- modules/users/tihdizer/dev/default.nix
+++ modules/users/tihdizer/dev/default.nix
@@ -3,9 +3,7 @@
   flake.modules.homeManager.hm-tihdizer-dev = {
     imports = with inputs.self.modules.homeManager; [
       hm-tihdizer-dev-zed-editor
-      hm-tihdizer-dev-go
       hm-tihdizer-dev-nix
-      hm-tihdizer-dev-python
       hm-tihdizer-dev-rust
       hm-tihdizer-dev-git
     ];
