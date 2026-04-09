8a099d6ce25e1a5f495a0023d3411b07a5a7dcb5
tihdizer <tihdizer@gmail.com>
2026-04-09 20:49:31 +0300

    chore: add todo comment

--- modules/users/tihdizer/shell/session-variables.nix
+++ modules/users/tihdizer/shell/session-variables.nix
@@ -3,6 +3,7 @@
   flake.modules.homeManager.hm-tihdizer-shell-session-variables =
     { ... }:
     {
+      # TODO: move in virtualisation
       home.sessionVariables = {
         LIBVIRT_DEFAULT_URI = "qemu:///system";
       };
