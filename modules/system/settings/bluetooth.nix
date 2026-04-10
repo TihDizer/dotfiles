07f801982b8cde98645a8e1acbf783f4230d4fd0
tihdizer <tihdizer@gmail.com>
2026-04-10 20:43:39 +0300

    refactor: move bluetuith in bluetooth

--- modules/system/settings/bluetooth.nix
+++ modules/system/settings/bluetooth.nix
@@ -1,11 +1,15 @@
 { ... }:
 {
   flake.modules.nixos.system-bluetooth =
-    { ... }:
+    { pkgs, ... }:
     {
       # Bluetooth
       hardware.bluetooth.enable = true;
       hardware.bluetooth.powerOnBoot = true;
+
+      environment.systemPackages = with pkgs; [
+        bluetuith
+      ];
 
       services.udev.extraRules = ''
         ACTION=="change", SUBSYSTEM=="bluetooth", ATTR{authorized}="1"
--- modules/users/tihdizer/utils/packages.nix
+++ modules/users/tihdizer/utils/packages.nix
@@ -4,19 +4,8 @@
     { pkgs, ... }:
     {
       home.packages = with pkgs; [
-        tray-tui # System tray TUI
-        rpm # RPM package manager
-        dpkg # Debian package manager
-        cpufetch # CPU architecture viewer
         fastfetch # System info display
 
-        # zoom-us # Zoom video conferencing
-        tty-clock # Terminal clock screensaver
-        clock-rs # Terminal clock (Rust)
-
-        jellyfin-ffmpeg # Jellyfin FFmpeg build
-
-        bluetuith # Bluetooth TUI manager
       ];
 
       programs.btop = {
