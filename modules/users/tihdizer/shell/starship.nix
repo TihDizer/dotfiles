68948ccf0599bb0286aa9a93b540cab6faaee314
tihdizer <tihdizer@gmail.com>
2026-04-17 21:21:57 +0300

    feat: zsh integration

--- modules/users/tihdizer/shell/starship.nix
+++ modules/users/tihdizer/shell/starship.nix
@@ -1,21 +1,36 @@
 { ... }:
 {
   flake.modules.homeManager.tihdizer-shell-starship =
-    { pkgs, ... }:
     {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      programs.fish.enable = lib.mkDefault true;
+      programs.zsh.enable = lib.mkDefault true;
+      programs.zsh.initContent = lib.mkAfter ''
+        ZLE_RPROMPT_INDENT=0
+      '';
+
       home.packages = with pkgs; [ starship ];
       programs.starship = {
         enable = true;
         package = pkgs.starship;
+        enableZshIntegration = config.programs.zsh.enable;
+        enableFishIntegration = config.programs.fish.enable;
         settings = {
           add_newline = false;
           command_timeout = 1300;
-          right_format = "$all";
+          right_format = "$nix_shell$all";
           format = "$shlvl$character";
           scan_timeout = 50;
           shell = {
             disabled = false;
             bash_indicator = "bsh";
+            zsh_indicator = "zsh";
+            fish_indicator = "fsh";
           };
           shlvl = {
             disabled = false;
@@ -25,7 +40,6 @@
             repeat_offset = 1;
           };
           character = {
-            # TODO: Stylix
             success_symbol = "❯";
             error_symbol = "";
             vimcmd_symbol = "❮";
