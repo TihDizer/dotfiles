57da26a80e7045219800811fb2ac06f59a6b0712
tihdizer <tihdizer@gmail.com>
2026-04-18 22:51:07 +0300

    fix: add lib

--- modules/users/tihdizer/shell/zsh.nix
+++ modules/users/tihdizer/shell/zsh.nix
@@ -1,7 +1,10 @@
 { ... }:
 {
   flake.modules.homeManager.tihdizer-shell-zsh =
-    { ... }:
+    {
+      lib,
+      ...
+    }:
     {
       programs.zsh.initContent = lib.mkAfter ''
         ZLE_RPROMPT_INDENT=0
