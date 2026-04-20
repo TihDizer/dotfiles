1e57808cebab2329c15e9b06e470a9584e7892d0
tihdizer <tihdizer@gmail.com>
2026-04-20 18:15:49 +0300

    feat: add scratchpad

---
+++ modules/users/tihdizer/niri/scratchpad.nix
@@ -0,0 +1,17 @@
+{
+  inputs,
+  ...
+}:
+{
+  flake-file.inputs = {
+    niri-scratchpad.url = "github:argosnothing/niri-scratchpad";
+  };
+
+  flake.modules.nixos.niri-scratchpad =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [
+        inputs.niri-scratchpad.packages.${pkgs.system}.default
+      ];
+    };
+}
