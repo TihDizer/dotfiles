c4e293fc1924a5ed3990bd13b5e9b54a73b37c07
tihdizer <tihdizer@gmail.com>
2026-05-30 18:50:05 +0300

    feat: add save position

--- modules/utils/mpv.nix
+++ modules/utils/mpv.nix
@@ -3,8 +3,14 @@
   flake.modules.homeManager.mpv =
     { pkgs, ... }:
     {
+      programs.mpv = {
+        enable = true;
+        config = {
+          save-position-on-quit = true;
+        };
+      };
+
       home.packages = with pkgs; [
-        mpv
         jellyfin-ffmpeg
       ];
     };
