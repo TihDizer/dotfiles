fcb6e1d4a746d13f181e4eadeae6972aa33c8429
tihdizer <tihdizer@gmail.com>
2026-06-04 12:39:10 +0300

    fix: app launcher

--- modules/utils/television.nix
+++ modules/utils/television.nix
@@ -15,16 +15,16 @@
         description = "Applications launcher"
 
         [source]
-        command = 'echo "$XDG_DATA_DIRS" | tr ":" "\n" | xargs -I {} find {}/applications -name "*.desktop" 2>/dev/null | xargs -I {} basename {} .desktop | sort -u'
+        command = 'printf "%s\n%s\n%s\n%s\n" "$XDG_DATA_DIRS" "$HOME/.local/share" "$HOME/.local/share/flatpak/exports/share" "/var/lib/flatpak/exports/share" | tr ":" "\n" | xargs -d "\n" -I {} find {}/applications -name "*.desktop" 2>/dev/null | xargs -d "\n" -I {} basename {} .desktop | sort -u'
 
         [preview]
-        command = 'echo "$XDG_DATA_DIRS" | tr ":" "\n" | xargs -I {} find {}/applications -name "{}.desktop" 2>/dev/null | head -n1 | xargs grep -E "^(Name|Comment|Exec)=" 2>/dev/null'
+        command = 'printf "%s\n%s\n%s\n%s\n" "$XDG_DATA_DIRS" "$HOME/.local/share" "$HOME/.local/share/flatpak/exports/share" "/var/lib/flatpak/exports/share" | tr ":" "\n" | xargs -d "\n" -I {} find {}/applications -name "{}.desktop" 2>/dev/null | head -n1 | xargs -d "\n" -I {} grep -E "^(Name|Comment|Exec)=" 2>/dev/null'
 
         [keybindings]
         enter = "actions:launch"
 
         [actions.launch]
-        command = "nohup gtk-launch '{}' > ~/.cache/television-launch.log 2>&1 &"
+        command = "XDG_DATA_DIRS=\"$HOME/.local/share:$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:$XDG_DATA_DIRS\" nohup gtk-launch '{}' > ~/.cache/television-launch.log 2>&1 &"
         mode = "execute"
       '';
 
@@ -34,16 +34,16 @@
         description = "Clipboard history manager (cliphist)"
 
         [source]
-        command = "cliphist list"
+        command = "sh -c 'res=$(cliphist list 2>/dev/null); if [ -z \"$res\" ]; then echo \"[Clipboard is empty]\"; else echo \"$res\"; fi'"
 
         [preview]
-        command = "echo '{}' | cliphist decode"
+        command = "sh -c 'tmp=$(mktemp); echo \"{}\" | cliphist decode > \"$tmp\" 2>/dev/null; if file --mime-type \"$tmp\" | grep -q \"image/\"; then chafa --size=60x30 \"$tmp\" 2>/dev/null || echo \"[Image: $(file -b \"$tmp\")]\"; else cat \"$tmp\"; fi; rm -f \"$tmp\"'"
 
         [keybindings]
         enter = "actions:copy"
 
         [actions.copy]
-        command = "echo '{}' | cliphist decode | wl-copy"
+        command = "echo '{}' | cliphist decode 2>/dev/null | wl-copy"
         mode = "execute"
       '';
 
