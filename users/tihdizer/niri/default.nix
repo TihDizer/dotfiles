07096d3ac70611640c5c7a41de1b8c5b904ae03c
tihdizer <tihdizer@gmail.com>
2026-06-07 16:48:56 +0300

    chore: try to fix screenshare

--- users/tihdizer/niri/default.nix
+++ users/tihdizer/niri/default.nix
@@ -30,11 +30,23 @@
         enable = true;
         extraPortals = with pkgs; [
           xdg-desktop-portal-gnome
-          # xdg-desktop-portal-gtk
-          # xdg-desktop-portal-wlr
+          xdg-desktop-portal-gtk
         ];
         config = {
-          common.default = [ "gnome" ];
+          common = {
+            default = [
+              "gnome"
+              "gtk"
+            ];
+          };
+          niri = {
+            default = [
+              "gnome"
+              "gtk"
+            ];
+            "org.freedesktop.impl.portal.Screencast" = [ "gnome" ];
+            "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
+          };
         };
         xdgOpenUsePortal = true;
       };
--- users/tihdizer/niri/startup.nix
+++ users/tihdizer/niri/startup.nix
@@ -9,7 +9,8 @@
         { sh = "xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 2"; }
         { sh = "sway-audio-idle-inhibit"; }
         { sh = "$POLKIT_BIN"; }
-        { sh = "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all"; }
+        # { sh = "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all"; }
+        { sh = "uwsm finalize"; }
         { command = [ "${pkgs.google-chrome}/bin/google-chrome" ]; }
         { command = [ "${pkgs.telegram-desktop}/bin/Telegram" ]; }
         { sh = "Throne"; }
