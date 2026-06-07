9b8320997d53ced4f9422155d3dace4a512e89eb
tihdizer <tihdizer@gmail.com>
2026-06-07 18:31:06 +0300

    chore: remove preview size

--- modules/dev/nvf.nix
+++ modules/dev/nvf.nix
@@ -100,8 +100,6 @@
                   mf.setup({
                     windows = {
                       preview = true,
-                      width_focus = 30,
-                      width_preview = 80,
                     },
                   })
                 '';
