f6958bb08d99f5ee20447a9e710f8decad4ca5e2
tihdizer <tihdizer@gmail.com>
2026-06-20 16:59:01 +0300

    fix: image preview

--- modules/utils/television.nix
+++ modules/utils/television.nix
@@ -30,14 +30,14 @@
 
       home.file.".config/television/cable/clip.toml".text = ''
         [metadata]
-        name = "clipboard"
+        name = "clip"
         description = "Clipboard history manager (cliphist)"
 
         [source]
         command = "cliphist list"
 
         [preview]
-        command = "sh -c 'tmp=''$(mktemp); cliphist decode ''$1 > ''$tmp 2>/dev/null; if file --mime-type ''$tmp | grep -q \"image/\"; then chafa --size=60x30 ''$tmp 2>/dev/null; else cat ''$tmp; fi; rm -f ''$tmp' -- {}"
+        command = "sh -c 'sz=''$(stty size </dev/tty 2>/dev/null || echo \"24 80\"); w=''$((''${sz#* } * 5 / 10)); h=''$((''${sz% *} - 4)); tmp=''$(mktemp); cliphist decode ''$1 > ''$tmp 2>/dev/null; if file --mime-type ''$tmp | grep -q \"image/\"; then chafa -f symbols --probe off --size=\"''${w}x''${h}\" ''$tmp 2>/dev/null; else cat ''$tmp; fi; rm -f ''$tmp' -- {}"
 
         [keybindings]
         enter = "actions:copy"
@@ -46,11 +46,5 @@
         command = "sh -c 'cliphist decode ''$1 | wl-copy' -- {}"
         mode = "execute"
       '';
-
-      home.shellAliases = {
-        tv-text = "tv text";
-        tv-apps = "tv apps";
-        tv-clip = "tv clipboard";
-      };
     };
 }
