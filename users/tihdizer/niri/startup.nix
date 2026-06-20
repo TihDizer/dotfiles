fe4dac6f87f42004aa071ea4ed63753541998497
tihdizer <tihdizer@gmail.com>
2026-06-20 13:46:54 +0300

    feat: add wl-clipboard

--- users/tihdizer/niri/startup.nix
+++ users/tihdizer/niri/startup.nix
@@ -9,6 +9,8 @@
         { sh = "xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 2"; }
         { sh = "sway-audio-idle-inhibit"; }
         { sh = "$POLKIT_BIN"; }
+        { sh = "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch cliphist store"; }
+        { sh = "${pkgs.wl-clipboard}/bin/wl-paste --type image --watch cliphist store"; }
         # { sh = "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all"; }
         { sh = "uwsm finalize"; }
         { command = [ "${pkgs.google-chrome}/bin/google-chrome" ]; }
