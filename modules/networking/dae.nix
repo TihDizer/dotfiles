38db31323c47fb63d59ba4d681bc077453cbee6c
tihdizer <tihdizer@gmail.com>
2026-06-27 13:02:21 +0300

    feat: add youtube rule

--- modules/networking/dae.nix
+++ modules/networking/dae.nix
@@ -10,7 +10,6 @@ let
       dial_mode: domain
       tls_implementation: tls
       tcp_check_url: 'http://cp.cloudflare.com,1.1.1.1,2606:4700:4700::1111'
-      tcp_check_http_method: HEAD
       udp_check_dns: 'dns.google:53,8.8.8.8,2001:4860:4860::8888'
       check_interval: 5m
       check_tolerance: 50ms
@@ -39,10 +38,18 @@ let
         policy: min_moving_avg
       }
 
-      germany_proxy {
+      germany {
         filter: name(regex: '.*\\xd0\\x93\\xd0\\xb5\\xd1\\x80\\xd0\\xbc\\xd0\\xb0\\xd0\\xbd\\xd0\\xb8\\xd1\\x8f.*')
         filter: name(regex: '.*🇩🇪.*')
         policy: min_moving_avg
+      }
+
+      youtube {
+        filter: subtag(proxy)
+        policy: min_moving_avg
+        tcp_check_url: 'https://www.youtube.com/generate_204,142.250.154.91,2a00:1450:4010:c0a::5b'
+        check_interval: 30s
+        check_tolerance: 50ms
       }
     }
 
@@ -60,10 +67,7 @@ let
       domain(nixos.org, nix.dev, search.nixos.org) -> direct
 
       #proxy
-      domain(geosite:youtube) -> proxy
       domain(geosite:discord) -> proxy
-      domain(suffix: discord.com, suffix: discordapp.com, suffix: discordapp.net, suffix: discord.gg) -> proxy
-      domain(suffix: discord.media, suffix: discordcdn.com, suffix: discordstatus.com) -> proxy
       domain(annas-archive.li, b4mcx2ml.net, britishcouncil.org, chatgpt.com, dashboard.kick.com, dub.co, givefreely.com, google.zoom.us, kick.com, linkedin.com, partners.dub.co, perplexity.ai, throne.me, trustedhousesitters.com, whatismyipaddress.com, zoom.us, app.zoom.us) -> proxy
       domain(suffix: amazonaws.com, suffix: b-cdn.net, suffix: throne.me) -> proxy
       domain(suffix: t.me, suffix: telegram.org, suffix: telegram.dog) -> proxy
@@ -72,8 +76,11 @@ let
       pname(Telegram) -> proxy
       pname(.Telegram-wrapped) -> proxy
 
-      #germany_proxy
-      domain(geosite:google) -> germany_proxy
+      #youtube
+      domain(geosite:youtube) -> youtube
+
+      #germany
+      domain(geosite:google) -> germany
 
       #direct
       fallback: direct
