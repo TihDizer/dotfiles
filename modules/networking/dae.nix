ac71bfd36d1543ac5bc2b44144df41d7f9f9bea2
tihdizer <tihdizer@gmail.com>
2026-08-19 15:46:48 +0300

    refactor(dae): change rule style

--- modules/networking/dae.nix
+++ modules/networking/dae.nix
@@ -13,10 +13,9 @@ let
       utls_imitate: chrome_auto
       tcp_check_url: 'http://cp.cloudflare.com,1.1.1.1'
       tcp_check_http_method: HEAD
-      # udp_check_dns: '8.8.8.8:53'
-      check_interval: 60s
+      udp_check_dns: '8.8.8.8:53'
+      check_interval: 30s
       check_tolerance: 50ms
-      disable_waiting_network: true
     }
 
     subscription {
@@ -24,18 +23,26 @@ let
     }
 
     dns {
-      ipversion_prefer: 4
       upstream {
         googledns: 'tcp+udp://8.8.8.8:53'
         landns: 'tcp+udp://77.88.8.8:53'
       }
       routing {
         request {
-          qtype(https) -> reject
-          qtype(aaaa) -> reject
-          qname(suffix: ru, suffix: su, suffix: xn--p1ai) -> landns
+          qname(
+            suffix: ru,
+            suffix: su,
+            suffix: xn--p1ai
+          ) -> landns
+
+          qtype(
+            aaaa,
+            https
+          ) -> reject
+
           fallback: googledns
         }
+
         response {
           fallback: accept
         }
@@ -49,64 +56,107 @@ let
       }
 
       germany {
-        filter: name(regex: '.*(Германия|🇩🇪).*') && !name(regex: '(?i).*(support|info|chat|канал|hysteria|grpc|⛔️).*')
-        policy: min_moving_avg
-      }
-
-      youtube {
-        filter: !name(regex: '(?i).*(support|info|chat|канал|hysteria|grpc|⛔️|россия).*') && !name(keyword: 'россия', '🇷🇺')
-        policy: min_moving_avg
-      }
-
-      kazakhstan {
-        filter: name(regex: '.*(Казахстан|🇰🇿).*') && !name(regex: '(?i).*(support|info|chat|канал|hysteria|grpc|⛔️).*')
+        filter: name(regex: '.*(Германия).*')
+          && !name(regex: '(?i).*(support|info|chat|канал|hysteria|grpc|⛔️).*')
         policy: min_moving_avg
       }
     }
 
     routing {
       #system
-      pname(NetworkManager) -> direct
-      dip(224.0.0.0/3, 'ff00::/8') -> direct
-      dip(geoip:private) -> direct
+      pname(
+        NetworkManager
+      ) -> direct
+
+      dip(
+        224.0.0.0/4,
+        geoip:private,
+        'ff00::/8'
+      ) -> direct
+
       l4proto(udp) && dport(443) -> block
       ipversion(6) -> block
 
-      dip(77.88.8.8) -> direct
-      dip(geoip:ru) -> direct
-      domain(suffix: ru, suffix: su, suffix: xn--p1ai) -> direct
-      domain(nixos.org, nix.dev, search.nixos.org) -> direct
+      dip(
+        77.88.8.8,
+        geoip:ru
+      ) -> direct
 
-      #youtube
-      domain(geosite:youtube) -> germany
-      domain(suffix: googlevideo.com, suffix: youtube.com, suffix: ytimg.com, suffix: youtu.be, geosite:youtube) -> germany
+      domain(
+        suffix: ru,
+        suffix: su,
+        suffix: xn--p1ai
+      ) -> direct
 
       #germany
-      domain(geosite:google) -> germany
-      domain(gemini.google.com, accounts.google.com, googleapis.com, gstatic.com, googleusercontent.com) -> germany
-      pname(agy) -> germany
+      domain(
+        geosite:google,
+        geosite:youtube
+      ) -> germany
 
-      #kazakhstan
-      domain(suffix: bybit.com, suffix: bybit.gl, suffix: bybit.biz, suffix: bybitglobal.com) -> kazakhstan
+      pname(
+        agy
+      ) -> germany
 
       #proxy
-      domain(geosite:discord) -> proxy
-      domain(annas-archive.li, b4mcx2ml.net, britishcouncil.org, chatgpt.com, dashboard.kick.com, dub.co, givefreely.com, google.zoom.us, kick.com, linkedin.com, partners.dub.co, perplexity.ai, throne.me, trustedhousesitters.com, whatismyipaddress.com, zoom.us, app.zoom.us) -> proxy
-      domain(suffix: amazonaws.com, suffix: b-cdn.net, suffix: throne.me) -> proxy
-      domain(suffix: t.me, suffix: telegram.org, suffix: telegram.dog, geosite: telegram) -> proxy
-      domain(suffix: speedtest.net) -> proxy
-      domain(suffix: 1flex.org, suffix: primevideo.com, suffix: roku.com, suffix: justwatch.com, suffix: ororo.tv, suffix: amazon.com) -> proxy
-      domain(suffix: kinozal.tv, kinozal.me, suffix: thepiratebay.org, suffix: rutor.org) -> proxy
-      domain(suffix: tor4me.info, suffix: tor2me.info, torrent4me.com, retracker.local, suffix: opentrackr.org, open.stealth.si, suffix: torrent.eu.org, tracker.bittor.pw, public.popcorn-tracker.org, tracker.dler.org, glotorrents.pw, tracker.coppersurfer.tk, torrent.gresille.org, p4p.arenabg.com, tracker.internetwarriors.net, exodus.desync.com, suffix: openbittorrent.com, suffix: leechers-paradise.org, suffix: cyberia.is) -> proxy
-      domain(suffix: jetbrains.com, suffix: openai.com, chatgpt.com, geosite: openai) -> proxy
-      domain(suffix: github.com) -> proxy
+      domain(
+        geosite:discord,
+        geosite:github,
+        geosite:openai,
+        geosite:telegram,
 
-      pname(Telegram) -> proxy
-      pname(.Telegram-wrapped) -> proxy
+        suffix: 1flex.org,
+        suffix: amazon.com,
+        suffix: amazonaws.com,
+        suffix: annas-archive.li,
+        suffix: b-cdn.net,
+        suffix: b4mcx2ml.net,
+        suffix: britishcouncil.org,
+        suffix: cyberia.is,
+        suffix: dub.co,
+        suffix: exodus.desync.com,
+        suffix: givefreely.com,
+        suffix: glotorrents.pw,
+        suffix: jetbrains.com,
+        suffix: justwatch.com,
+        suffix: kick.com,
+        suffix: kinozal.me,
+        suffix: kinozal.tv,
+        suffix: leechers-paradise.org,
+        suffix: linkedin.com,
+        suffix: nixos.org,
+        suffix: open.stealth.si,
+        suffix: openbittorrent.com,
+        suffix: opentrackr.org,
+        suffix: ororo.tv,
+        suffix: p4p.arenabg.com,
+        suffix: perplexity.ai,
+        suffix: primevideo.com,
+        suffix: public.popcorn-tracker.org,
+        suffix: retracker.local,
+        suffix: roku.com,
+        suffix: rutor.org,
+        suffix: speedtest.net,
+        suffix: thepiratebay.org,
+        suffix: throne.me,
+        suffix: tor2me.info,
+        suffix: tor4me.info,
+        suffix: torrent.eu.org,
+        suffix: torrent.gresille.org,
+        suffix: torrent4me.com,
+        suffix: tracker.bittor.pw,
+        suffix: tracker.coppersurfer.tk,
+        suffix: tracker.dler.org,
+        suffix: tracker.internetwarriors.net,
+        suffix: trustedhousesitters.com,
+        suffix: whatismyipaddress.com,
+        suffix: zoom.us
+      ) -> proxy
 
-      #direct
-      pname(.transmission-q) -> direct
-      domain(suffix: local, keyword: torrent) -> direct
+      pname(
+        .Telegram-wrapped,
+        Telegram
+      ) -> proxy
 
       #fallback
       fallback: direct
