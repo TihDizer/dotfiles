8688ccbdcac7023931471bd9fcb06d7eb478e62c
tihdizer <tihdizer@gmail.com>
2026-08-27 01:15:07 +0300

    fix(niri): use full polkit-gnome path and remove undefined env var

--- users/tihdizer/niri/startup.nix
+++ users/tihdizer/niri/startup.nix
@@ -5,10 +5,9 @@
     {
       programs.niri.settings.spawn-at-startup = [
         { sh = "systemctl --user reset-failed"; }
-        { sh = "polkit-gnome-authentication-agent-1"; }
+        { command = [ "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1" ]; }
         { sh = "xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 2"; }
         { sh = "sway-audio-idle-inhibit"; }
-        { sh = "$POLKIT_BIN"; }
         { sh = "${pkgs.wl-clipboard}/bin/wl-paste --type text --watch cliphist store"; }
         { sh = "${pkgs.wl-clipboard}/bin/wl-paste --type image --watch cliphist store"; }
         # { sh = "${pkgs.dbus}/bin/dbus-update-activation-environment --systemd --all"; }
