de04eaa5093524c2cf2830d64fb62d75cbb2be38
tihdizer <tihdizer@gmail.com>
2026-08-27 22:09:54 +0300

    feat(niri): configure swaylock-effects and add wlogout package

--- users/tihdizer/niri/default.nix
+++ users/tihdizer/niri/default.nix
@@ -26,7 +26,20 @@
         };
       };
 
-      programs.hyprlock.enable = true;
+      programs.swaylock = {
+        enable = true;
+        package = pkgs.swaylock-effects;
+        settings = {
+          screenshots = true;
+          clock = true;
+          indicator = true;
+          indicator-radius = 100;
+          indicator-thickness = 7;
+          effect-blur = "7x5";
+          effect-vignette = "0.5:0.5";
+          fade-in = 0.2;
+        };
+      };
 
       xdg.portal = {
         enable = true;
@@ -73,7 +86,7 @@
         xprop
         xkbcomp
         # Wayland desktop (niri)
-        swaylock-effects # Screen locker с эффектами
+        wlogout
       ];
     };
 }
