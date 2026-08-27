dee0d802c1dbf0fd6094bc8bfaf55c5d0d4cd19a
tihdizer <tihdizer@gmail.com>
2026-08-27 21:30:05 +0300

    refactor(shell): remove virtualization session variable now in qemu module

--- users/tihdizer/shell/session-variables.nix
+++ users/tihdizer/shell/session-variables.nix
@@ -3,9 +3,7 @@
   flake.modules.homeManager.tihdizer-session-variables =
     { ... }:
     {
-      # TODO: move in virtualisation
       home.sessionVariables = {
-        LIBVIRT_DEFAULT_URI = "qemu:///system";
       };
     };
 }
