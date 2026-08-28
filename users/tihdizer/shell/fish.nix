cf754576ce6dc1d924f674ba6c8d2d085ac01542
tihdizer <tihdizer@gmail.com>
2026-08-28 17:20:36 +0300

    feat(fish): set fastfetch as interactive shell greeting

--- users/tihdizer/shell/fish.nix
+++ users/tihdizer/shell/fish.nix
@@ -3,12 +3,15 @@
   flake.modules.homeManager.tihdizer-fish =
     { ... }:
     {
-      programs.fish.interactiveShellInit = ''
-        fish_vi_key_bindings
-        set fish_cursor_default block blink
-        set fish_cursor_insert line blink
-        set fish_cursor_replace_one underscore blink
-        set fish_cursor_visual block
-      '';
+      programs.fish = {
+        functions.fish_greeting = "fastfetch";
+        interactiveShellInit = ''
+          fish_vi_key_bindings
+          set fish_cursor_default block blink
+          set fish_cursor_insert line blink
+          set fish_cursor_replace_one underscore blink
+          set fish_cursor_visual block
+        '';
+      };
     };
 }
