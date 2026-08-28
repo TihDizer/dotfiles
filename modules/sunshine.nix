4aaa523cb45823cd31635fbc74c9934b0b274e4f
tihdizer <tihdizer@gmail.com>
2026-08-28 22:53:13 +0300

    feat(sunshine): run as system service for pre-login remote streaming

--- modules/sunshine.nix
+++ modules/sunshine.nix
@@ -1,24 +1,17 @@
 { ... }:
 {
   flake.modules.nixos.sunshine =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
     {
       environment.systemPackages = with pkgs; [
         moonlight-qt # Client
       ];
 
-      systemd.user.services.sunshine = {
-        serviceConfig = {
-          TimeoutStopSec = "5s";
-          ExecStartPre = "-${pkgs.procps}/bin/pkill -u %u -x sunshine";
-        };
-      };
-
       services.sunshine = {
         enable = true;
         openFirewall = true;
         capSysAdmin = true;
-        autoStart = true;
+        autoStart = false;
 
         settings = {
           capture = "kms";
@@ -34,6 +27,23 @@
               name = "Desktop";
             }
           ];
+        };
+      };
+
+      systemd.services.sunshine = {
+        description = "Sunshine self-hosted game stream host (System Service)";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${config.services.sunshine.package}/bin/sunshine ${config.services.sunshine.settings.file_apps}";
+          Restart = "always";
+          RestartSec = "3s";
+          AmbientCapabilities = [ "CAP_SYS_ADMIN" ];
+          CapabilityBoundingSet = [ "CAP_SYS_ADMIN" ];
+          User = "root";
         };
       };
     };
