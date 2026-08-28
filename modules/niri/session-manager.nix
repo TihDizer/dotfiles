4db026f7f758bd3d4437a7d506ca66f6e6386d32
tihdizer <tihdizer@gmail.com>
2026-08-28 23:46:52 +0300

    feat(greetd): replace tuigreet with direct autologin session via uwsm

--- modules/niri/session-manager.nix
+++ modules/niri/session-manager.nix
@@ -3,7 +3,6 @@
   flake.modules.nixos.programs-desktop-niri-session-manager =
     { pkgs, lib, ... }:
     let
-      tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
       niri-session = "${pkgs.niri}/bin/niri-session";
     in
     {
@@ -19,15 +18,15 @@
           binPath = niri-session;
         };
       };
-      #|==< TuiGreet >==|#
+      #|==< Greetd Auto-Login >==|#
       security.pam.services.greetd.enableGnomeKeyring = true;
       services.greetd = {
         enable = true;
         settings = {
           terminal.vt = lib.mkForce 7;
           default_session = {
-            command = "${tuigreet} --time --remember --asterisks --container-padding 2 --no-xsession-wrapper --cmd '${pkgs.uwsm}/bin/uwsm start niri-session'";
-            user = "greeter";
+            command = "${pkgs.uwsm}/bin/uwsm start niri-session";
+            user = "tihdizer";
           };
         };
       };
