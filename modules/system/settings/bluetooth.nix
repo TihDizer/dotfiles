e1f08e48998ad22be0ae2a35f3660bd97b706bcb
tihdizer <tihdizer@gmail.com>
2026-08-31 17:13:33 +0300

    fix(bluetooth): remove somo experimental options

--- modules/system/settings/bluetooth.nix
+++ modules/system/settings/bluetooth.nix
@@ -7,15 +7,11 @@
       hardware.bluetooth.powerOnBoot = true;
       hardware.bluetooth.settings = {
         General = {
-          Experimental = true;
-          FastConnectable = true;
-          PairableTimeout = 0;
           JustWorksRepairing = "always";
+          PairableTimeout = 0;
         };
         Policy = {
           AutoEnable = true;
-          ReconnectAttempts = 7;
-          ReconnectIntervals = "1, 2, 4, 8, 16, 32, 64";
         };
       };
 
