9f169e7f8a9c99b6617949a03692d1b7f56fd9c2
tihdizer <tihdizer@gmail.com>
2026-09-10 18:28:16 +0300

    feat(tihdizer): add some pkgs

--- users/tihdizer/packages.nix
+++ users/tihdizer/packages.nix
@@ -9,6 +9,8 @@
         antigravity-cli # AI code assistant
         gtypist # Touch typing tutor
         timr-tui # TUI to organize your time: Pomodoro, Tabata, Countdown, Timer, Event
+        code2prompt # Context engineering tool
+        readest # E-book reader
       ];
     };
 }
