2d9de0e84fff780f0b5df42940340f7c2344778f
tihdizer <tihdizer@gmail.com>
2026-09-10 16:39:21 +0300

    feat(audio): rename hd audio devices

--- modules/system/settings/audio.nix
+++ modules/system/settings/audio.nix
@@ -8,6 +8,75 @@
         enable = true;
         pulse.enable = true;
         wireplumber.enable = true;
+        wireplumber.extraConfig."50-rename-hd-audio" = {
+          "monitor.alsa.rules" = [
+            {
+              matches = [
+                {
+                  "device.name" = "alsa_card.pci-0000_30_00.6";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "device.description" = "Realtek ALC897 Audio";
+                  "device.nick" = "Realtek ALC897";
+                };
+              };
+            }
+            {
+              matches = [
+                {
+                  "node.name" = "alsa_output.pci-0000_30_00.6.analog-stereo";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "node.description" = "Realtek ALC897 Analog Stereo";
+                  "node.nick" = "Speakers / Headphones";
+                };
+              };
+            }
+            {
+              matches = [
+                {
+                  "node.name" = "alsa_input.pci-0000_30_00.6.analog-stereo";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "node.description" = "Realtek ALC897 Microphone";
+                  "node.nick" = "Microphone";
+                };
+              };
+            }
+            {
+              matches = [
+                {
+                  "device.name" = "alsa_card.pci-0000_30_00.1";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "device.description" = "AMD Cezanne HDMI/DP Audio";
+                  "device.nick" = "Cezanne HDMI";
+                };
+              };
+            }
+            {
+              matches = [
+                {
+                  "node.name" = "alsa_output.pci-0000_30_00.1.hdmi-stereo";
+                }
+              ];
+              actions = {
+                update-props = {
+                  "node.description" = "AMD Cezanne HDMI/DP Audio Digital Stereo (HDMI)";
+                  "node.nick" = "Cezanne HDMI";
+                };
+              };
+            }
+          ];
+        };
         alsa.enable = true;
         alsa.support32Bit = true;
         extraConfig.pipewire."10-virtual-cable" = {
