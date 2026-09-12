5370ce32756b6acd66d31d7ddcef5b6168e9ee49
tihdizer <tihdizer@gmail.com>
2026-09-12 16:18:07 +0300

    fix(bluetooth): reconnect issue

--- modules/system/settings/bluetooth.nix
+++ modules/system/settings/bluetooth.nix
@@ -7,13 +7,28 @@
       hardware.bluetooth.powerOnBoot = true;
       hardware.bluetooth.settings = {
         General = {
+          FastConnectable = true;
           JustWorksRepairing = "always";
           PairableTimeout = 0;
+          Experimental = true;
+          MultiProfile = "multiple";
         };
         Policy = {
           AutoEnable = true;
+          ReconnectAttempts = 7;
+          ReconnectIntervals = "1, 2, 4, 8, 16, 32, 64";
         };
       };
+
+      hardware.bluetooth.input = {
+        General = {
+          ClassicBondedOnly = false;
+          IdleTimeout = 0;
+          UserspaceHID = true;
+        };
+      };
+
+      boot.kernelModules = [ "uhid" ];
 
       hardware.enableAllFirmware = true;
 
